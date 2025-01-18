b84b3a67996164e8ae23f6efdd99363a236414b8
Sebastarazq <juansebastianhijomenor@hotmail.com>
2025-01-17 23:23:31 -0500

    Obtener Empleados y listarlos

---
+++ FrontCrudEmpleados/Entities/DTOs/EmpleadosDTO.cs
@@ -0,0 +1,22 @@
+﻿namespace FrontCrudEmpleados.Entities.DTOs
+{
+    public class EmpleadosDTO
+    {
+        public string Id { get; set; }
+        public string NombreCompleto { get; set; }
+        public string Cargo { get; set; }
+        public int Edad { get; set; }
+        public string? UrlFoto { get; set; }
+        public DireccionDTO Direccion { get; set; }
+        public bool Active { get; set; }
+    }
+
+    public class DireccionDTO
+    {
+        public string Pais { get; set; }
+        public string Linea1 { get; set; }
+        public string Linea2 { get; set; }
+        public string Ciudad { get; set; }
+        public string Departamento { get; set; }
+    }
+}
